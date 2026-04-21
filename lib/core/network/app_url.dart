acff0d1141aec3c438c3f86209cb4f4945ed734a
Rakibul-E404 <smrat246@gmail.com>
2026-04-21 15:06:17 +0600

    all ok

--- lib/core/network/app_url.dart
+++ lib/core/network/app_url.dart
@@ -1,8 +1,8 @@
 class AppUrl {
   AppUrl._();
 
-  static const String baseUrl = 'https://mohaimin8001.sobhoy.com/api/v1';
-  static const String imageBaseUrl = 'https://mohaimin8001.sobhoy.com';
+  static const String baseUrl = 'http://206.162.244.11:8001/api/v1';
+  static const String imageBaseUrl = 'http://206.162.244.11:8001';
   static const String registration = '$baseUrl/auth/register';
   static const String resendEmailOtp = '$baseUrl/auth/register';///---> tis will chane
   static const String verifyEmailOtp = '$baseUrl/auth/verify-email';
